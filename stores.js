433d882e2123289e733ac668dac245b23cf6114e
Elena Teitelbaum <47948415+ElenaKishko@users.noreply.github.com>
2023-07-28 03:26:53 +0300

    Delete stores.js

--- stores.js
+++
@@ -1,4 +0,0 @@
-export { FilterStore } from "./FilterStore";
-export { CartStore } from "./CartStore";
-export { FiltersStore } from "./FiltersStore";
-export { app, storage, auth, db } from "./firebase";
